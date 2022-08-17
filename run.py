3291e8989b929584f890d355a3917c848608a968
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-17 14:41:40 +0000

    Add basic madlib functionality to run.py

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!!"
+
+print(madlib)
